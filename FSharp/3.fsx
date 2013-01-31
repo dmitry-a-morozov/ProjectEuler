5012ff6fba45802ae4fafd547b372e7ad3603879
Dmitry <dmitry.a.morozov@gmail.com>
2013-01-31 16:40:22 -0500

    problem 3 fixed

--- FSharp/3.fsx
+++ FSharp/3.fsx
@@ -1,1 +1,7 @@
 ﻿
+let rec largestFactor x factor = 
+    if factor = x then factor 
+    elif x % factor = 0L then largestFactor (x / factor) factor 
+    else largestFactor x (factor + 1L)
+
+largestFactor 600851475143L 2L
