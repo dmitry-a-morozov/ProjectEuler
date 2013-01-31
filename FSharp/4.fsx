5bc0842a6eba56e9a0c1b5ca9e7789869a0ad3b6
Dmitry <dmitry.a.morozov@gmail.com>
2013-01-31 16:46:02 -0500

    problem 4

---
+++ FSharp/4.fsx
@@ -0,0 +1,15 @@
+﻿
+let reverse x = 
+    let rec loop rem acc = 
+        if rem = 0 then acc 
+        else loop (rem / 10) (acc * 10 + (rem % 10))
+    loop x 0
+    
+[
+    for x in 100..999 do
+        for y in 100..999 do
+            let product = x * y
+            let isPalindrome = product = reverse product
+            if isPalindrome then yield product 
+]
+|> List.max 
