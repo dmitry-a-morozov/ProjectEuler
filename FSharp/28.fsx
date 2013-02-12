cb461d750cc2e22bdb4264f1f97f849cb9dae221
Dmitry <dmitry.a.morozov@gmail.com>
2013-02-11 23:10:43 -0500

    problem 28

---
+++ FSharp/28.fsx
@@ -0,0 +1,10 @@
+﻿
+{ 3 .. 2 .. 1001 }
+|> Seq.collect (fun side ->
+    seq {
+        yield side * side;
+        yield side * side - side - 1;
+        yield side * side - (side - 1) * 2;
+        yield side * side - (side - 1) * 3;
+    })
+|> Seq.fold (+) 1
