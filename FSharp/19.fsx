3e9fe582cf5157e9eae9030957b6450a3407bcb2
Dmitry <dmitry.a.morozov@gmail.com>
2013-02-13 14:05:13 -0500

    problem 25

--- FSharp/19.fsx
+++ FSharp/19.fsx
@@ -1,6 +1,6 @@
 ﻿open System
 
-DateTime(1901,1,1).AddMonths
+DateTime(1901, 1, 1).AddMonths
 |> Seq.initInfinite 
 |> Seq.takeWhile(fun date -> date <= DateTime(2000,12,31))
 |> Seq.filter (fun x -> x.DayOfWeek = DayOfWeek.Sunday)
---
+++ FSharp/25.fsx
@@ -0,0 +1,6 @@
+﻿
+(1I, 1I) 
+|> Seq.unfold (fun(current, next) -> Some(current, (next, current + next)))
+|> Seq.takeWhile (fun x -> x / 10I ** (1000 - 1) = 0I)
+|> Seq.length 
+|> (+) 1
