c110b7f3ef6725a95643fc9ca887bba6bf5debb1
Dmitry <dmitry.a.morozov@gmail.com>
2013-02-13 15:16:22 -0500

    problem 22

---
+++ FSharp/22.fsx
@@ -0,0 +1,10 @@
+﻿
+seq {
+    let wc = new System.Net.WebClient()
+    let data = wc.DownloadString("http://projecteuler.net/project/names.txt")
+    for s in data.Split(',') -> s.Trim('"')
+}
+|> Seq.sort
+|> Seq.map (fun name -> List.sum [for c in name -> int c - int 'A' + 1])
+|> Seq.mapi (fun pos value -> (pos + 1) * value)
+|> Seq.sum
