36b096f1020ff6d65657a486dc8fa26da7e5d4c6
Dmitry <dmitry.a.morozov@gmail.com>
2013-02-13 15:28:48 -0500

    problem 22

--- FSharp/22.fsx
+++ FSharp/22.fsx
@@ -5,6 +5,8 @@ seq {
     for s in data.Split(',') -> s.Trim('"')
 }
 |> Seq.sort
-|> Seq.map (fun name -> List.sum [for c in name -> int c - int 'A' + 1])
+|> Seq.map (fun name -> query { for c in name do sumBy (int c - int 'A' + 1) })
 |> Seq.mapi (fun pos value -> (pos + 1) * value)
 |> Seq.sum
+
+
